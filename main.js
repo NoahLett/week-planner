56383d975cdb286e4c1a66d53d601703dab1cada
Noah Lett <noahblett96@gmail.com>
2022-10-31 23:04:36 +0000

    Project-Noah Lett

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
+
 var $modal = document.querySelector('.modal');
 var $addWindowEntry = document.querySelector('.add-window-entry');
-var $modalOff = document.querySelector('.modal-off');
-function modalOn() {
-  var modalTurnOn;
-  $modal.className = 'modal ' + modalTurnOn;
+// var $modalOff = document.querySelector('.modal-off');
+
+$modal.addEventListener('click', modalOn);
+
+function modalOn(event) {
   $addWindowEntry.className = 'add-window-entry';
 }
-$modal.addEventListener('click', modalOn);
-function addWindowEntryOn() {
-  $addWindowEntry.className = 'add-window-entry ' + 'hidden';
-}
-$modalOff.addEventListener('click', addWindowEntryOn);
+
+// function modalOn() {
+//   var modalTurnOn;
+//   $modal.className = 'modal ' + modalTurnOn;
+//   $addWindowEntry.className = 'add-window-entry';
+// }
+// $modal.addEventListener('click', modalOn);
+// // function addWindowEntryOn() {
+// //   $addWindowEntry.className = 'add-window-entry ' + 'hidden';
+// // }
+// // // $modalOff.addEventListener('click', addWindowEntryOn);
